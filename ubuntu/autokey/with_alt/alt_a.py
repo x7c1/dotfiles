7d50dc8dd4a5c7ebb5d6f66a1e1a487c6d4a3c18
x7c1 <jmtaro+x7c1@gmail.com>
2019-03-11 23:18:50 +0900

    add with_alt

---
+++ ubuntu/autokey/with_alt/alt_a.py
@@ -0,0 +1,4 @@
+store.set_global_value('hotkey', '<alt>+a')
+engine.set_return_value('<ctrl>+a')
+engine.run_script('combo')
+
---
+++ ubuntu/autokey/with_alt/alt_f.py
@@ -0,0 +1,4 @@
+store.set_global_value('hotkey', '<alt>+f')
+engine.set_return_value('<ctrl>+f')
+engine.run_script('combo')
+
