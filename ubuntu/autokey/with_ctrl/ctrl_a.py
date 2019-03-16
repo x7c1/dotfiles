d39f67885a907694c817c816d1094e935ae90425
x7c1 <jmtaro+x7c1@gmail.com>
2019-03-16 21:36:37 +0900

    add with_ctrl

---
+++ ubuntu/autokey/with_ctrl/ctrl_a.py
@@ -0,0 +1,4 @@
+store.set_global_value('hotkey', '<ctrl>+a')
+engine.set_return_value('<home>')
+engine.run_script('combo')
+
---
+++ ubuntu/autokey/with_ctrl/ctrl_d.py
@@ -0,0 +1,4 @@
+store.set_global_value('hotkey', '<ctrl>+d')
+engine.set_return_value('<delete>')
+engine.run_script('combo')
+
