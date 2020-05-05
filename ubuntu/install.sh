8ccf1c22535219a754e2aeb3f0bfcb782026b309
x7c1 <jmtaro+x7c1@gmail.com>
2020-05-06 00:47:37 +0900

    add ubuntu/install.sh

---
+++ ubuntu/install.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+
+set -x
+set -u
+set -e
+
+ubuntu_root="$(pwd)"/ubuntu
+shared_dir="$(pwd)"/shared
+
+main() {
+  #sudo apt update
+  install_packages
+
+  setup_git_config
+  setup_tmux
+  setup_vim
+  setup_zsh
+  setup_peco
+}
+
+install_packages() {
+  sudo apt install -y \
+    tig \
+    tree
+}
+
+setup_git_config() {
+  git config --global interactive.singlekey true
+  sudo apt install libterm-readkey-perl
+}
+
+setup_tmux() {
+  sudo apt install tmux
+
+  [ -e ~/.tmux.conf ] || \
+    ln -s "$shared_dir"/.tmux.conf ~
+}
+
+setup_vim() {
+  sudo apt install -y \
+    neovim \
+    curl
+
+  [ -e ~/.config/nvim ] || \
+    ln -s "$ubuntu_root"/nvim ~/.config
+
+  if [ -d ~/.cache/dein ]; then
+    echo "dein already installed."
+    echo "delete ~/.cache/dein before re-installing."
+    return
+  fi
+
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh \
+    > dein-installer.sh
+
+  sh ./dein-installer.sh ~/.cache/dein
+}
+
+setup_zsh() {
+  sudo apt install zsh
+
+  touch ~/.local.zshrc
+  echo 'export PATH=${HOME}/bin:$PATH' >> ~/.local.zshrc
+
+  if [ -d ~/.oh-my-zsh ]; then
+    echo "oh-my-zsh already installed."
+    echo "delete ~/.oh-my-zsh before re-installing."
+  else
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    mv ~/.zshrc ~/.oh-my-zsh.zshrc.backup
+  fi
+
+  [ -e ~/.zshrc ] || \
+    ln -s "$shared_dir"/.zshrc ~
+
+  [ -e ~/.common.zshrc ] || \
+    ln -s "$shared_dir"/.common.zshrc ~
+
+  [ -e ~/.oh-my-zsh.zshrc ] || \
+    ln -s "$shared_dir"/.oh-my-zsh.zshrc ~
+}
+
+setup_peco() {
+  [ -d ~/bin ] || \
+    mkdir ~/bin
+
+  if [ -e ~/bin/peco ]; then
+    echo "peco already installed."
+    echo "delete ~/bin/peco before re-installing."
+  else
+    # see latest version at https://github.com/peco/peco/releases
+    curl -L -O https://github.com/peco/peco/releases/download/v0.5.7/peco_linux_amd64.tar.gz
+    tar -xvf peco_linux_amd64.tar.gz
+    mv peco_linux_amd64/peco ~/bin/peco
+  fi
+  # see shared/.peco.zshrc
+  touch ~/.cd_history_file
+
+  [ -e ~/.peco ] || \
+    ln -s "$shared_dir"/.peco ~
+
+  [ -e ~/.peco.zshrc ] || \
+    ln -s "$shared_dir"/.peco.zshrc ~
+}
+
+main
+
