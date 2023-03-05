c9241fa8c81d504623030b0c06b1b812d91b01a2
x7c1 <jmtaro+x7c1@gmail.com>
2023-03-05 21:09:36 +0900

    add thumbs.sh

---
+++ ubuntu/thumbs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -xue
+
+files=$@
+
+height=2160
+width=$(($height * 2 / 3))
+
+feh -L '%w %h %f' $files \
+  | awk '{ if ($1 < $2) { print $0 } }' \
+  | cut -d ' ' -f 3- \
+  | shuf \
+  | tail -7 \
+  | feh \
+    --borderless \
+    --thumbnails \
+    --index-info '' \
+    --image-bg white \
+    --thumb-width $(($width - 5)) \
+    --thumb-height $(($height - 5)) \
+    --limit-height $(($height)) \
+    --no-menus \
+    --full-screen \
+    --randomize \
+    --filelist -
+
