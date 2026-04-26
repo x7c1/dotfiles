09f3a654f52953863a6fbfd58c1bc55ec72887be
x7c1 <994424+x7c1@users.noreply.github.com>
2026-04-27 01:20:47 +0900

    feat: revert tmux pane title when a command fails
    preexec sets the title tentatively to the new command name; precmd
    checks $? and either commits the new title on success or restores the
    previous one on failure. Avoids leaving the tab labeled with a typo or
    a command that never actually ran.

--- config/zsh/tmux-title.zsh
+++ config/zsh/tmux-title.zsh
@@ -1,14 +1,30 @@
-# Track the last command in the pane title so tmux #T (used by catppuccin
-# window tabs) shows what was last run instead of the hostname.
+# Track the last successfully-run command in the pane title so tmux #T
+# (used by catppuccin window tabs) shows what was last run instead of
+# the hostname. Failed commands do not stick.
 autoload -U add-zsh-hook
 
-_tmux_title_init() {
-  print -Pn '\e]2;zsh\a'
-  add-zsh-hook -d precmd _tmux_title_init
-}
-add-zsh-hook precmd _tmux_title_init
+typeset -g _tmux_title=""
+typeset -g _tmux_pending=""
 
 _tmux_title_preexec() {
-  print -Pn "\e]2;${1%% *}\a"
+  _tmux_pending="${1%% *}"
+  print -Pn "\e]2;${_tmux_pending}\a"
 }
 add-zsh-hook preexec _tmux_title_preexec
+
+_tmux_title_precmd() {
+  local exit_status=$?
+  if [[ -z "$_tmux_title" ]]; then
+    _tmux_title="zsh"
+    print -Pn "\e]2;${_tmux_title}\a"
+  fi
+  if [[ -n "$_tmux_pending" ]]; then
+    if [[ $exit_status -eq 0 ]]; then
+      _tmux_title="$_tmux_pending"
+    else
+      print -Pn "\e]2;${_tmux_title}\a"
+    fi
+    _tmux_pending=""
+  fi
+}
+add-zsh-hook precmd _tmux_title_precmd
